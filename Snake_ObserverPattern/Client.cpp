a62a9f7c1147683ea2660ce274eef2eae691939a
AxelOlofEriksson <axeloloferiksson@hotmail.com>
2020-09-09 13:30:59 +0200

    Update Client.cpp

--- Snake_ObserverPattern/Client.cpp
+++ Snake_ObserverPattern/Client.cpp
@@ -4,7 +4,7 @@ void Client::update(float score)
 {
 	// Print the changed values
 	std::cout << "---Observer (" << id << ") Data---\tScore: " << score << '\n';
-
+	std::cout<< "hej";
 }
 
 Client::Client(int id) 
